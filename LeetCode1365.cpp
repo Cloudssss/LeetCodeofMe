b9fe3f87b0816d4309913ab6e94675f41bb9bd19
Pixuan Li <Cloudlpx@Gmail.com>
2020-10-26 13:02:03 +0800

    leetcode1365

---
+++ LeetCode1365.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<pair<int,int>> data;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            data.emplace_back(nums[i],i);
+        }
+
+        sort(data.begin(),data.end());
+
+        vector<int> ret(n,0);
+        int prev = -1;
+        for (int i = 0; i < n; ++i) {
+            if (prev == -1 || data[i].first!=data[i-1].first)
+            {
+                prev = i;
+            }
+            ret[data[i].second] = prev;
+        }
+        return ret;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
