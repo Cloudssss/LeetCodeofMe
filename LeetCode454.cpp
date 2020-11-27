23c7910ff330ab7852233c62c4fcfeb1723f77f2
Pixuan Li <Cloudlpx@Gmail.com>
2020-11-27 22:11:20 +0800

    leetcode454

---
+++ LeetCode454.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int,int> countAB;
+        for (auto& a : A){
+            for (auto& b : B){
+                ++countAB[a+b];
+            }
+        }
+
+        int ans = 0;
+        for (auto& c : C){
+            for (auto& d : D){
+                if (countAB.count(-c-d)){
+                    ans += countAB[-c-d];
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
